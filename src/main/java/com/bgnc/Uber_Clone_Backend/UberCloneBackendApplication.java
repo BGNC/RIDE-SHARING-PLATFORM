9189480c098131f168d860809c0508d5bdb563d0
BGNC <bugra34055@hotmail.com>
2024-11-21 01:41:32 +0300

    Initial commit

---
+++ src/main/java/com/bgnc/Uber_Clone_Backend/UberCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bgnc.Uber_Clone_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberCloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgnc/Uber_Clone_Backend/UberCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgnc.Uber_Clone_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
